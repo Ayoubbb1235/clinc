48e4aa22a0050f6418f826ebb27a16a22b69e86d
Ayoubbb1235 <159105060+Ayoubbb1235@users.noreply.github.com>
2024-12-29 03:13:22 +0100

    Add files via upload

--- med.js
+++ med.js
@@ -1,10 +1,34 @@
 document.addEventListener('scroll',()=> {
-    const header = document.querySelector('header');
     if(window.scrollY > 300){
         header.classList.add('scrolled');
 
     } else{
         header.classList.remove('scrolled');
     }
+}
+);
+const header = document.querySelector('header');
+
+const menu = document.querySelector('.menubutton');
+const time = document.querySelector('.times');
+
+menu.addEventListener('click',()=> {
+    const nav = document.querySelector('.navmobile');
+
+    menu.classList.add('mobile');
+    nav.classList.remove('mobile');
+
+    
+}
+);
+
+
+time.addEventListener('click',()=> {
+    const nav = document.querySelector('.navmobile');
+
+    nav.classList.add('mobile');
+    nav.classList.remove('mobile');
+
+    
 }
 );
